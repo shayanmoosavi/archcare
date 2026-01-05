4062e6b4465ec29f101a42e6a26b0f73382aebf8
shayanmoosavi <moosavi.shayan@gmail.com>
2026-01-05 15:51:08 +0330

    added HealthCheckTask to the module re-exports

--- src/archcare/tasks/__init__.py
+++ src/archcare/tasks/__init__.py
@@ -6,5 +6,6 @@ Each module in this package implements specific maintenance tasks.
 
 from .base import BaseTask
 from .failed_services import FailedServicesTask
+from .health_check import HealthCheckTask
 
-__all__ = ["BaseTask", "FailedServicesTask"]
+__all__ = ["BaseTask", "FailedServicesTask", "HealthCheckTask"]
