657695de16d749aaa03e64cabcab5a060a6869b1
shayanmoosavi <moosavi.shayan@gmail.com>
2026-01-07 22:21:32 +0330

    Added MirrorlistUptateTask to the module re-exports

--- src/archcare/tasks/__init__.py
+++ src/archcare/tasks/__init__.py
@@ -7,5 +7,6 @@ Each module in this package implements specific maintenance tasks.
 from .base import BaseTask
 from .failed_services import FailedServicesTask
 from .health_check import HealthCheckTask
+from .mirrorlist_update import MirrorlistUpdateTask
 
-__all__ = ["BaseTask", "FailedServicesTask", "HealthCheckTask"]
+__all__ = ["BaseTask", "FailedServicesTask", "HealthCheckTask", "MirrorlistUpdateTask"]
