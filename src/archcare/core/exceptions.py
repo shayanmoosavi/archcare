1b2c11b8c733e367b20b9e8cc7be690e30a2ffe0
shayanmoosavi <moosavi.shayan@gmail.com>
2026-07-23 18:29:42 +0330

    added domain exceptions for core layer

---
+++ src/archcare/core/exceptions.py
@@ -0,0 +1,11 @@
+"""Domain exceptions for the Archcare core layer."""
+
+from archcare.exceptions import ArchcareError
+
+
+class ArchcareCoreError(ArchcareError):
+    """Base class for domain exceptions raised by the core layer."""
+
+
+class TaskNotRegisteredError(ArchcareCoreError):
+    """Raised when a task name has no corresponding class in the TaskRegistry."""
